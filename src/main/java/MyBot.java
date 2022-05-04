17d8554b3499aa472747e62be80ac4921a517e5d
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-05-04 14:25:09 +0200

    Modificado MyBot.java (2)

--- src/main/java/MyBot.java
+++ src/main/java/MyBot.java
@@ -3,10 +3,12 @@ import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInsta
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
@@ -23,6 +25,7 @@ import java.io.*;
 import reactor.core.publisher.Mono;
 
 import javax.swing.*;
+import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
@@ -66,82 +69,97 @@ public class MyBot {
         //returns an authorized Credential object.
         return credential;
     }
+
     public static void main(String[] args) {
         //Utilizamos el DiscordClient, que es la clase necesaria para interacutar con Discord
         //creamos uno con el token de nuestro bot, el cual obtuve en el portal para desarrolladores de Discord, al crear el bot en mi aplicación
-        final String token = "OTUzNjMzMDMyMDg5MjcyMzQw.YjHZ-A.6Kz4b8QD4RV9OJp2PA0bcBzSaFA";
+        final String token = "OTUzNjMzMDMyMDg5MjcyMzQw.YjHZ-A.COFNVxvsyfl7kGN2GPNFA5AA608";
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
-        //constructor del embed
-        EmbedCreateSpec embed = EmbedCreateSpec.builder()
-                .color(Color.CINNABAR)
-                .title("Bobby")
-                .image("https://i.ytimg.com/vi/M6W9dPBthAU/hqdefault.jpg")
-                .build();
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT;
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                    .setApplicationName(APPLICATION_NAME)
+                    .build();
+            //constructor del embed
+            EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                    .color(Color.CINNABAR)
+                    .title("Bobby")
+                    .image("https://i.ytimg.com/vi/M6W9dPBthAU/hqdefault.jpg")
+                    .build();
 
-        //creacion del evento escuchador de mensajes
-        gateway.on(MessageCreateEvent.class).subscribe(event -> {
-            final Message message = event.getMessage();
-            //declaracion de los comandos
-            if ("!ping".equals(message.getContent())) {
-                final MessageChannel channel = message.getChannel().block();
-                //con esta línea el bot muestra un mensaje con los que sele indica
-                channel.createMessage("Pong!").block();
-            }
-            if ("!embed".equals(message.getContent())) {
-                final MessageChannel channel = message.getChannel().block();
-                //un mensaje del bot con un parámetro diferente (embed)
-                channel.createMessage(MessageCreateSpec.builder()
-                        .content("Sigo?")
-                        .addEmbed(embed)
-                        .build()).subscribe();
-            }
-            if ("/list".equals((message.getContent()))) {
-                final MessageChannel channel = message.getChannel().block();
-
-                String ruta = "src/main/images";
-                File carpeta = new File(ruta);
-                //con File.list() se listan los contenidos del directorio
-                String[] imagenes = carpeta.list();
-                String res = "";
-                for (int i = 0; i < imagenes.length; i++) {
-                    res += imagenes[i]+"\n";
+            //creacion del evento escuchador de mensajes
+            gateway.on(MessageCreateEvent.class).subscribe(event -> {
+                final Message message = event.getMessage();
+                //declaracion de los comandos
+                if ("!ping".equals(message.getContent())) {
+                    final MessageChannel channel = message.getChannel().block();
+                    //con esta línea el bot muestra un mensaje con los que sele indica
+                    channel.createMessage("Pong!").block();
                 }
-
-                channel.createMessage(res).block();
-            }
-            if (message.getContent().startsWith("/get")) {
-                final MessageChannel channel = message.getChannel().block();
-                //con un substring recojo solo el texto que haya después del comando /get
-                String archivo = message.getContent().substring(5, message.getContent().length());
-                //declaracion de un nuevo embed
-                EmbedCreateSpec embed2 = EmbedCreateSpec.builder()
-                        .color(Color.BISMARK)
-                        .title(archivo.split("\\.")[0])
-                        .image("attachment://src/main/images/"+archivo)
-                        .build();
-                //registramos el archivo que vamos a mostrar en el nuevo embed
-                InputStream fileAsInputStream = null;
-                boolean exists = true;
-                try {
-                    fileAsInputStream = new FileInputStream("src/main/images/"+archivo);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    exists = false;
-                }
-                if (exists == true) {
+                if ("!embed".equals(message.getContent())) {
+                    final MessageChannel channel = message.getChannel().block();
+                    //un mensaje del bot con un parámetro diferente (embed)
                     channel.createMessage(MessageCreateSpec.builder()
-                            .addFile("src/main/images/"+archivo, fileAsInputStream)
-                            .addEmbed(embed2)
+                            .content("Sigo?")
+                            .addEmbed(embed)
                             .build()).subscribe();
-                } else {
-                    channel.createMessage("El archivo no existe").block();
                 }
+                if ("/list".equals((message.getContent()))) {
+                    final MessageChannel channel = message.getChannel().block();
 
-            }
-        });
+                    String ruta = "src/main/images";
+                    File carpeta = new File(ruta);
+                    //con File.list() se listan los contenidos del directorio
+                    String[] imagenes = carpeta.list();
+                    String res = "";
+                    for (int i = 0; i < imagenes.length; i++) {
+                        res += imagenes[i]+"\n";
+                    }
 
-        gateway.onDisconnect().block();
+                    channel.createMessage(res).block();
+                }
+                if (message.getContent().startsWith("/get")) {
+                    final MessageChannel channel = message.getChannel().block();
+                    //con un substring recojo solo el texto que haya después del comando /get
+                    String archivo = message.getContent().substring(5, message.getContent().length());
+                    //declaracion de un nuevo embed
+                    EmbedCreateSpec embed2 = EmbedCreateSpec.builder()
+                            .color(Color.BISMARK)
+                            .title(archivo.split("\\.")[0])
+                            .image("attachment://src/main/images/"+archivo)
+                            .build();
+                    //registramos el archivo que vamos a mostrar en el nuevo embed
+                    InputStream fileAsInputStream = null;
+                    boolean exists = true;
+                    try {
+                        fileAsInputStream = new FileInputStream("src/main/images/"+archivo);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        exists = false;
+                    }
+                    if (exists == true) {
+                        channel.createMessage(MessageCreateSpec.builder()
+                                .addFile("src/main/images/"+archivo, fileAsInputStream)
+                                .addEmbed(embed2)
+                                .build()).subscribe();
+                    } else {
+                        channel.createMessage("El archivo no existe").block();
+                    }
+
+                }
+            });
+
+            gateway.onDisconnect().block();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
